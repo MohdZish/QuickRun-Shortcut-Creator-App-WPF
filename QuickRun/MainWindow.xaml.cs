db9b1c31ab9f8509809db5de43492927f12cbe9b
MohdZish <mohammed-zishan.abdul_wahab@edu.devinci.fr>
2020-07-21 12:10:58 +0200

    Cards added

--- QuickRun/MainWindow.xaml.cs
+++ QuickRun/MainWindow.xaml.cs
@@ -47,9 +47,44 @@ namespace QuickRun
         private void Test(object sender, RoutedEventArgs e)
         {
             string[] readText = File.ReadAllLines(@"C:\test\test.txt");
-            foreach (string s in readText)
+            foreach (string mon in readText)
             {
-                Console.WriteLine(s);
+                Button namebutton = new Button();
+                StackPanel insidebutton = new StackPanel();
+                insidebutton.HorizontalAlignment = HorizontalAlignment.Left;
+                insidebutton.VerticalAlignment = VerticalAlignment.Top;
+                insidebutton.Height = 80;
+
+                Label itemname = new Label();
+                TextBlock itemnameblock = new TextBlock();
+                itemname.Content = itemnameblock;
+                itemnameblock.Text = mon;
+                itemnameblock.Tag = mon;
+                itemnameblock.TextWrapping = TextWrapping.Wrap;
+                itemname.Foreground = Brushes.White;
+                itemname.HorizontalAlignment = HorizontalAlignment.Center;
+                itemname.VerticalContentAlignment = VerticalAlignment.Center;
+                itemname.HorizontalContentAlignment = HorizontalAlignment.Center;
+                itemname.Height = 70;
+                itemname.Width = 80;
+                Ellipse typecolor = new Ellipse();
+                typecolor.Height = 5;
+                typecolor.Width = 5;
+
+                namebutton.Content = insidebutton;
+                namebutton.BorderThickness = new Thickness(1);
+                namebutton.Background = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+                namebutton.BorderBrush = new SolidColorBrush(Color.FromArgb(0, 0, 0, 255));
+                namebutton.Margin = new Thickness(5);
+                namebutton.Width = 80;
+                namebutton.Height = 80;
+                namebutton.Tag = mon;
+                namebutton.Width = 80;
+                namebutton.Height = 80;
+                insidebutton.Children.Add(typecolor);
+                insidebutton.Children.Add(itemname);
+                MyPanel.Children.Add(namebutton);
+
             }
         }
 
